61110a3002423edd5d0811e3166acc5d8301eb9a
bugsyy157 <it.b.60.abhishek.yadav@gmail.com>
2022-10-04 21:02:09 +0530

    enhanced the portfolio site

--- portfolio site/src/components/about/About.js
+++ portfolio site/src/components/about/About.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import TabPanel from './TabPanel'
 
-import Img1 from '../../images/Hero-bg2.jpg'
+import Img1 from '../../images/Hero-bg1.png'
 
 function About() {
     
@@ -15,7 +15,7 @@ function About() {
 
     return (
         <>
-            <section className='py-5' style={parallex}>
+            <section className='py-5' id='about' style={parallex}>
                 <div className="container">
                     <h1 className='text-center' style={{fontWeight: 'bolder', color: 'rgb(203, 178, 106)'}}>About Me</h1>
                     <div className='container'>
